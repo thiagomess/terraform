aad23708622faf4b7279e7017a8ae7fca4611b05
Thiago <thiagogomes19@hotmail.com>
2020-05-24 22:00:05 -0300

    Utilizando referências e dependências entre os recursos

--- main.tf
+++ main.tf
@@ -11,7 +11,28 @@ resource "aws_instance" "dev" {
   tags = {
       Name = "dev${count.index}"
   }
-  vpc_security_group_ids = ["sg-0b07f3c3cce30da72", "sg-17cedd34"]
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+  ami = "ami-085925f297f89fce1"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = ["${aws_s3_bucket.dev4}"] #cria uma dependencia com o recurso s3 Bucket
+}
+
+resource "aws_instance" "dev5" {
+  ami = "ami-085925f297f89fce1"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+      Name = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"] #vinculando por referencia
 }
 
 resource "aws_security_group" "acesso-ssh" {
@@ -29,5 +50,14 @@ resource "aws_security_group" "acesso-ssh" {
 
   tags = {
     Name = "allow_ssh"
+  }
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "thiago-dev4"
+  acl    = "private"
+
+  tags = {
+    Name = "thiago-dev3"
   }
 }
